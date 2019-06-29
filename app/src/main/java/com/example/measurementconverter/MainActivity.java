e17f4975eefed1483d8aff850f497836f11ace4b
abdiwali olad <abdiwaliolad@gmail.com>
2019-06-29 14:43:26 -0700

    add all fields

--- app/src/main/java/com/example/measurementconverter/MainActivity.java
+++ app/src/main/java/com/example/measurementconverter/MainActivity.java
@@ -1,23 +1,30 @@
 package com.example.measurementconverter;
-
-import android.graphics.Interpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] conversion = { "Miles to Kilometers", "Kilometers to Mile ", "Inches to Centimeters", "Centimeters to Inches" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Spinner  mySpinner = (Spinner) findViewById(R.id.spinner1);
-
-
-        ArrayAdapter<CharSequence> myAdapter = ArrayAdapter.createFromResource(this, R.array.conversion));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter((myAdapter));
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, conversion);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(), "Conversion: "+conversion[position] ,Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO - Custom Code
     }
 }
